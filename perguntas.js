64bea4c178411dbf30796290f6dff03905135e2f
3AVTH035 <00001084568433sp@al.educacao.sp.gov.br>
2024-11-25 21:41:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    '√25?',
+    '5'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica o Brasil?',
+    'América do Sul'
+)
+
+criaCartao(
+    'Quimica',
+    'H2O é a formula do q?',
+    'Da Agua '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O q significa a Palavra Door?',
+    'Porta'
+)
